0337abb04abf2066e44d2f2014d8935e2011b572
o Rafa <paracafa@gmail.com>
2017-09-17 13:21:08 -0300

    Exercício revisado e finalizado.

--- exercicio/app/src/main/java/com/example/orafa/exercicio/MainActivity.java
+++ exercicio/app/src/main/java/com/example/orafa/exercicio/MainActivity.java
@@ -6,31 +6,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonToast = (Button)findViewById(R.id.butao1);
-        Button buttonTela = (Button)findViewById(R.id.butao2);
-        final EditText editText1 = (EditText)findViewById(R.id.textoedit);
+        this.mViewHolder.butao1 = (Button) findViewById(R.id.butao1);
+        this.mViewHolder.butao2 = (Button) findViewById(R.id.butao2);
+        this.mViewHolder.textoedit = (EditText) findViewById(R.id.textoedit);
 
-        buttonToast.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Toast.makeText(getApplication(), editText1.getText().toString(), Toast.LENGTH_LONG ).show();
-            }
-        });
+        this.mViewHolder.butao1.setOnClickListener(this);
+        this.mViewHolder.butao2.setOnClickListener(this);
+    }
 
-        buttonTela.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplication(), SegundaTela.class);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.butao1:
+                Toast.makeText(getApplication(), mViewHolder.textoedit.getText().toString(), Toast.LENGTH_LONG).show();
+                break;
+            case R.id.butao2:
+                Intent intent = new Intent(this, SegundaTela.class);
                 startActivity(intent);
-            }
-        });
+                break;
+        }
+    }
+
+    private static class ViewHolder {
+        Button butao1;
+        Button butao2;
+        EditText textoedit;
     }
 }
